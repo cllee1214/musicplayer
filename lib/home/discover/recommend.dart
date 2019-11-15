92bd16325ea50c857177bdf02919eea308989295
yunhong.cai <yunhong.cai@1905.com>
2019-11-15 16:41:49 +0800

    完成推荐歌单数据渲染

--- lib/home/discover/recommend.dart
+++ lib/home/discover/recommend.dart
@@ -1,56 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class RecommendList extends StatelessWidget {
-  const RecommendList({Key key}) : super(key: key);
+import '../../widgets/loading.dart';
+
+class RecommendList extends StatefulWidget {
+  RecommendList({Key key}) : super(key: key);
+
+  _RecommendListState createState() => _RecommendListState();
+}
+
+class _RecommendListState extends State<RecommendList> {
+
+  List recommendListData;
+  bool hasLoad = false; 
+
+  getData () async{
+    Dio dio = Dio();
+    var res = await dio.get('http://192.168.18.2:3000/personalized?limit=6');
+    if(res.data['code'] == 200){
+       recommendListData = res.data['result'];
+      setState(() {
+        hasLoad = true;
+      });
+    }else{
+      hasLoad = false;
+      print('get banner error...');
+    }
+  }
+
+  @override
+  void initState() {   
+    super.initState();
+    getData();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Colors.red,
-              child:  Text('推荐歌单',textAlign: TextAlign.left,),
-            ),
-           
-           
-            SizedBox(
-              height: 360,
-              // color: Colors.grey,
-              child: GridView.count(
-                crossAxisCount: 3,
-                crossAxisSpacing: 5,
-                // mainAxisSpacing: 3,
-                childAspectRatio: 0.7,
-                physics: new NeverScrollableScrollPhysics(),
-                children: <Widget>[
-                  PlayListItem(),
-                  PlayListItem(),
-                   PlayListItem(),
-
-                    PlayListItem(),
-                     PlayListItem(),
-                      PlayListItem()
-              ],
-            ),
-            )
-          ],
-          
-        ),
+            child: hasLoad ? Column(
+              children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        color: Colors.red,
+                        child:  Text('推荐歌单',textAlign: TextAlign.left,),
+                      ),
+                      SizedBox(
+                        height: 360,
+                        // color: Colors.grey,
+                        child: GridView.count(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 5,
+                          // mainAxisSpacing: 3,
+                          childAspectRatio: 0.7,
+                          physics: new NeverScrollableScrollPhysics(),
+                          children: recommendListData.map((data) => PlayListItem(imageUrl: data['picUrl'], name: data['name'])).toList()
+                      ),
+                      )
+                    ],
+            ): Loading(),
     );
   }
 }
 
-//349368004
 
 class PlayListItem extends StatefulWidget {
-  PlayListItem({Key key}) : super(key: key);
+  String imageUrl;
+  String name;
+  PlayListItem({Key key, @required this.imageUrl, @required this.name}) : super(key: key);
 
   _PlayListItemState createState() => _PlayListItemState();
 }
 
 class _PlayListItemState extends State<PlayListItem> {
+
+  @override
+  void initState() { 
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -59,10 +87,10 @@ class _PlayListItemState extends State<PlayListItem> {
                 ClipRRect(
                   borderRadius: BorderRadius.circular(5),
                   child: Container(
-                    child:  Image.network('http://p1.music.126.net/i0qi6mibX8gq2SaLF1bYbA==/2002210674180198.jpg'),
+                    child:  Image.network(widget.imageUrl),
                   )
                 ),
-                Text('那些好听到发疯的粤语歌', style: TextStyle(fontSize: 12))
+                Text(widget.name,overflow: TextOverflow.ellipsis, maxLines: 2, style: TextStyle(fontSize: 12))
               ],
             ),
       );
